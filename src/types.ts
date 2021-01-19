98c0ae3865091d1a11e679a54cd76d08db9c6a95
Floris Bernard <floris.bernard@gmail.com>
2021-01-19 12:41:04 +0100

    Generate flattened type to get deep value types

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,31 @@
+type UnionToIntersection<U> = (U extends any
+  ? (k: U) => void
+  : never) extends ((k: infer I) => void)
+  ? I
+  : never;
+
+/**
+ * Returns the intersection of all values of the object T. Example:
+ * ```
+ * IntersectValuesOf<{ foo: Foo, bar: Bar }> = Foo & Bar
+ * ```
+ */
+type IntersectValuesOf<T> = UnionToIntersection<T[keyof T]>;
+
 type AddPrefix<TKey extends string, Prefix> = Prefix extends string ? `${Prefix}.${TKey}` : TKey;
 
-export type GenerateDotNotation<Schema, Prefix = null> = {
-  [K in keyof Schema]: AddPrefix<`${string & K}`, Prefix> | (
-    Schema[K] extends Array<any> ? never : (
-      Schema[K] extends object ?
-        /* recursion is allowed in conditional branches from TS 4.1 onwards */
-        /* see: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-1.html#recursive-conditional-types */
-        GenerateDotNotation<Schema[K], AddPrefix<`${string & K}`, Prefix>> :
-        never
-    )
-  );
-}[keyof Schema];
+/**
+ * Flattens an object type to a single object with dot notation for keys. Example:
+ * ```
+ * DotNotationMap<{ foo: { bar: number }, foo2: string }> = { foo: { bar: number }, "foo.bar": number, foo2: string }
+ * ```
+ */
+export type FlattenedWithDotNotation<Schema, Prefix = null> =
+  /* first, simply output the keys with prefix and their types: */
+  { [K in string & keyof Schema as AddPrefix<K, Prefix>]: Schema[K] } & // see https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-1.html#key-remapping-in-mapped-types
+  /* then, for each sub-object, recurse */
+  IntersectValuesOf<{
+    [K in string & keyof Schema as AddPrefix<K, Prefix>]: Schema[K] extends Array<any> ?
+      never :
+      Schema[K] extends object ? FlattenedWithDotNotation<Schema[K], AddPrefix<K, Prefix>> : never
+  }>;
