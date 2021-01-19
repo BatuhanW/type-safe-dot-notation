f3015ba5c7987f2bd0d0ae0fcdf3ab97a7b25c77
Floris Bernard <floris.bernard@gmail.com>
2021-01-19 12:18:00 +0100

    Use recursive types with TS 4.1

--- src/example.ts
+++ src/example.ts
@@ -21,20 +21,22 @@ const exampleSchema = {
   key03: 44,
 };
 
-const key00: GenerateDotNotation<typeof exampleSchema> = "key00";
-const key0011: GenerateDotNotation<typeof exampleSchema> = "key00.key11";
-const key0012: GenerateDotNotation<typeof exampleSchema> = "key00.key12";
-const key0013: GenerateDotNotation<typeof exampleSchema> = "key00.key13";
-const key0014: GenerateDotNotation<typeof exampleSchema> = "key00.key14";
-const key001421: GenerateDotNotation<typeof exampleSchema> = "key00.key14.key21";
-const key001422: GenerateDotNotation<typeof exampleSchema> = "key00.key14.key22";
-const key001433: GenerateDotNotation<typeof exampleSchema> = "key00.key14.key23";
-const key001424: GenerateDotNotation<typeof exampleSchema> = "key00.key14.key24";
-const key00142431: GenerateDotNotation<typeof exampleSchema> = "key00.key14.key24.key31";
-const key00142432: GenerateDotNotation<typeof exampleSchema> = "key00.key14.key24.key32";
-const key00142433: GenerateDotNotation<typeof exampleSchema> = "key00.key14.key24.key33";
-const key01: GenerateDotNotation<typeof exampleSchema> = "key01";
-const key02: GenerateDotNotation<typeof exampleSchema> = "key02";
-const key03: GenerateDotNotation<typeof exampleSchema> = "key03";
+type DotNotation = GenerateDotNotation<typeof exampleSchema>;
 
-const invalidKey: GenerateDotNotation<typeof exampleSchema> = "";
+const key00: DotNotation = "key00";
+const key0011: DotNotation = "key00.key11";
+const key0012: DotNotation = "key00.key12";
+const key0013: DotNotation = "key00.key13";
+const key0014: DotNotation = "key00.key14";
+const key001421: DotNotation = "key00.key14.key21";
+const key001422: DotNotation = "key00.key14.key22";
+const key001433: DotNotation = "key00.key14.key23";
+const key001424: DotNotation = "key00.key14.key24";
+const key00142431: DotNotation = "key00.key14.key24.key31";
+const key00142432: DotNotation = "key00.key14.key24.key32";
+const key00142433: DotNotation = "key00.key14.key24.key33";
+const key01: DotNotation = "key01";
+const key02: DotNotation = "key02";
+const key03: DotNotation = "key03";
+
+const invalidKey: DotNotation = "";
--- src/types.ts
+++ src/types.ts
@@ -1,43 +1,13 @@
-type LevelOne<Schema> = {
-  [K in keyof Schema]: `${string & K}` | LevelTwo<Schema, K>;
+type AddPrefix<TKey extends string, Prefix> = Prefix extends string ? `${Prefix}.${TKey}` : TKey;
+
+export type GenerateDotNotation<Schema, Prefix = null> = {
+  [K in keyof Schema]: AddPrefix<`${string & K}`, Prefix> | (
+    Schema[K] extends Array<any> ? never : (
+      Schema[K] extends object ?
+        /* recursion is allowed in conditional branches from TS 4.1 onwards */
+        /* see: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-1.html#recursive-conditional-types */
+        GenerateDotNotation<Schema[K], AddPrefix<`${string & K}`, Prefix>> :
+        never
+    )
+  );
 }[keyof Schema];
-
-type LevelTwo<Schema, K extends keyof Schema> = {
-  [L in keyof Schema[K]]: Schema[K] extends Array<any>
-    ? never
-    : Schema[K] extends object
-    ? `${string & K}.${string & L}` | LevelThree<Schema, K, L>
-    : never;
-}[keyof Schema[K]];
-
-type LevelThree<Schema, K extends keyof Schema, L extends keyof Schema[K]> = {
-  [M in keyof Schema[K][L]]: Schema[K][L] extends Array<any>
-    ? never
-    : Schema[K][L] extends object
-    ? `${string & K}.${string & L}.${string & M}` | LevelFour<Schema, K, L, M>
-    : never;
-}[keyof Schema[K][L]];
-
-type LevelFour<Schema, K extends keyof Schema, L extends keyof Schema[K], M extends keyof Schema[K][L]> = {
-  [N in keyof Schema[K][L][M]]: Schema[K][L][M] extends Array<any>
-    ? never
-    : Schema[K][L][M] extends object
-    ? `${string & K}.${string & L}.${string & M}.${string & N}` | LevelFive<Schema, K, L, M, N>
-    : never;
-}[keyof Schema[K][L][M]];
-
-type LevelFive<
-  Schema,
-  K extends keyof Schema,
-  L extends keyof Schema[K],
-  M extends keyof Schema[K][L],
-  N extends keyof Schema[K][L][M]
-> = {
-  [O in keyof Schema[K][L][M][N]]: Schema[K][L][M][N] extends Array<any>
-    ? never
-    : Schema[K][L][M][N] extends object
-    ? `${string & K}.${string & L}.${string & M}.${string & N}.${string & O}`
-    : never;
-}[keyof Schema[K][L][M][N]];
-
-export type GenerateDotNotation<Schema> = LevelOne<Schema>;
